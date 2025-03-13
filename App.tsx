29bee35016911da7e1aa47cb8a84a5012492ced4
Everton Cruz <cruz.everton@gmail.com>
2025-03-13 17:48:04 -0300

    removed unusable code from App.tsx

--- App.tsx
+++ App.tsx
@@ -8,12 +8,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <TaskProvider>
-      {/* <TaskListScreen /> */}
-      {/* <View style={styles.container}> */}
-      {/* <Text>Open up App.tsx to start working on your app!</Text> */}
-      {/* <StatusBar style="auto" /> */}
       <AppNavigator />
-      {/* </View> */}
     </TaskProvider>
   );
 }
